54e15ecdaf904853463ea2e54954168217ab1369
Ronnie Nissan <ronni.n.yousif@gmail.com>
2020-04-21 12:11:53 +0300

    added the highlight patch

--- config.def.h
+++ config.def.h
@@ -4,13 +4,15 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Hack:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#8fbcbb", "#3b4252" },
+	[SchemeSel] = { "#3b4252", "#8fbcbb" },
+	[SchemeSelHighlight] = { "#bf616a", "#8fbcbb" },
+	[SchemeNormHighlight] = { "#bf616a", "#3b4252" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
